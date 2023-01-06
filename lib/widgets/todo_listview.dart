81fcc1b1e9f6b744ec824af99074c73b59175a6b
david1707 <DavidAlacant@gmail.com>
2023-01-06 13:11:42 +0100

    Confirm Dialog before delete item. Still needs cleaning old code and styling

--- lib/widgets/todo_listview.dart
+++ lib/widgets/todo_listview.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/todo_item.dart';
-import './todo_listtile.dart';
+import 'todo_listtile.dart';
 
 class TodoListView extends StatefulWidget {
   final List<TodoItem> todoList;
@@ -27,12 +27,34 @@ class _MyWidgetState extends State<TodoListView> {
       itemBuilder: (context, index) {
         return Dismissible(
           key: Key(todoList[index].id),
-          confirmDismiss: (direction) =>
-              widget.confirmDismiss(direction, index),
+          confirmDismiss: (direction) {
+            return showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                title: const Text('Delete todo'),
+                content: const Text('Confirm delete this todo'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: const Text('Close'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                    child: const Text('Delete'),
+                  ),
+                ],
+              ),
+            );
+          },
           child: ToDoListTile(
             todo: todoList[index],
             toogleFinished: widget.toogleFinished,
           ),
+          onDismissed: (_) {},
         );
       },
       itemCount: todoList.length,
